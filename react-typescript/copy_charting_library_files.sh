fe31c9cd5b2dba3bdc472155c5d2888d3cb28925
xlorne <1991wangliang@gmail.com>
2023-12-09 15:05:14 +0800

    fix react copy_charting_library_files.sh

--- react-typescript/copy_charting_library_files.sh
+++ react-typescript/copy_charting_library_files.sh
@@ -4,22 +4,24 @@ remove_if_directory_exists() {
 	if [ -d "$1" ]; then rm -Rf "$1"; fi
 }
 
-BRANCH="master";
+BRANCH="main";
+REPOSITORY='git@github.com:xlorne/charting_library.git'
 
-REPOSITORY='https://github.com/tradingview/charting_library/'
+# Using a fixed directory name for cloning
+CLONE_DIR="./charting_library"
 
-LATEST_HASH=$(git ls-remote $REPOSITORY $BRANCH | grep -Eo '^[[:alnum:]]+')
+remove_if_directory_exists "$CLONE_DIR"
 
-remove_if_directory_exists "$LATEST_HASH"
-
-git clone -q --depth 1 -b "$BRANCH" $REPOSITORY "$LATEST_HASH"
+# Cloning into a fixed directory
+git clone -q --depth 1 -b "$BRANCH" $REPOSITORY "$CLONE_DIR"
 
 remove_if_directory_exists "public/charting_library"
 remove_if_directory_exists "public/datafeeds"
 remove_if_directory_exists "src/charting_library"
 
-cp -r "$LATEST_HASH/charting_library" public
-cp -r "$LATEST_HASH/datafeeds" public
-cp -r "$LATEST_HASH/charting_library" src
+# Ensure these paths are correct relative to your script's location
+cp -r "$CLONE_DIR/charting_library" public
+cp -r "$CLONE_DIR/datafeeds" public
+cp -r "$CLONE_DIR/charting_library" src
 
-remove_if_directory_exists "$LATEST_HASH"
+remove_if_directory_exists "$CLONE_DIR"
